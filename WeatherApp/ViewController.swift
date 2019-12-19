60f5e6369fd2e1ee35a71e3815d4110ef0211f97
vesnna <vesna.nikita@yandex.ru>
2019-12-19 18:20:50 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Nikita Vesna on 19.12.2019.
+//  Copyright © 2019 Nikita Vesna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
